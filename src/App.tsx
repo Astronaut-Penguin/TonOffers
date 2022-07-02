8f001fe0d2c524247aad87ef5cbcaed0ea9b3f3d
Alejo Viola <viola.alejo@outlook.com>
2022-07-02 12:53:48 -0300

    [App] - Restructure

--- src/App.tsx
+++ src/App.tsx
@@ -18,38 +18,45 @@ import {
 import { Gallery, Publication } from './views';
 
 // Data JSON7
-import data from "../src/data/data.json"
+import data from '../src/data/data.json';
 
 const App: React.FC = () => {
 	return (
 		<>
-			{/* HEADER */}
-			<Header />
+			<Router>
+				{/* HEADER */}
+				<Header />
 
-			{/* APP CONTAINER */}
-			<div className={styles.Container_App}>
-				<Router>
+				{/* APP CONTAINER */}
+				<div className={styles.Container_App}>
 					<Switch>
+						{/* GALLERY VIEW */}
 						<Route path="/" element={<Gallery />} />
+
+						{/* PUBLICATION VIEW */}
 						{data.data.map(function (value, i, a) {
-					return (
-						<Route path={"/publication/" + value.route} element={
-						<Publication 
-							profile={value.profile}
-							activity={value.activity}
-							name={value.name}
-							min={value.price.min}
-							max={value.price.max}
-							coin={value.price.coin}
-							description={value.description}
-							image={value.images.perfil}
-							images={value.images.works}
-							/>} />
-					);
-				})} 
+							return (
+								<Route
+									path={'/publication/' + value.route}
+									element={
+										<Publication
+											profile={value.profile}
+											activity={value.activity}
+											name={value.name}
+											min={value.price.min}
+											max={value.price.max}
+											coin={value.price.coin}
+											description={value.description}
+											image={value.images.perfil}
+											images={value.images.works}
+										/>
+									}
+								/>
+							);
+						})}
 					</Switch>
-				</Router>
-			</div>
+				</div>
+			</Router>
 		</>
 	);
 };
